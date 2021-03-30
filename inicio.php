aacd9b4456f34f959056900cfcf6be75357817c7
Mario Sanchez <mario.sanchez@ontwice.com>
2021-03-30 15:08:03 -0600

    Inicio del proyecto

---
+++ inicio.php
@@ -0,0 +1,5 @@
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit suscipit sequi id libero perferendis corrupti asperiores nostrum provident impedit reiciendis officia fugit velit voluptatibus, magnam at nam, aspernatur cum amet.
+
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat at repellendus molestiae voluptate voluptatem veritatis, atque soluta alias ad asperiores adipisci perspiciatis maxime tenetur debitis! Exercitationem laboriosam unde corporis similique!
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex praesentium tempora laborum facilis aperiam quaerat fugiat alias culpa unde, molestiae asperiores molestias eius error illo perspiciatis, blanditiis et libero qui.
